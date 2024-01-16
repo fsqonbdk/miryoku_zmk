65d0e6cbd3486b19b6c62b71607192a154d10af2
fsqonbdk <50521462+fsqonbdk@users.noreply.github.com>
2024-01-16 09:23:36 -0800

    LAYER_NUM replace &kp GRAVE with &kp SLASH

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -106,10 +106,11 @@ U_NP,              U_NP,              &kp C_MUTE,        &kp C_PP,          &kp 
 
 // #define MIRYOKU_ALTERNATIVES_NUM_FLIP \
 
+// replace &kp GRAVE with &kp SLASH
 #define MIRYOKU_LAYER_NUM \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            &kp SEMI,          \
-U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp GRAVE,         \
+U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp SLASH,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp N0,            &kp DOT,           U_NP,              U_NP
 
 
